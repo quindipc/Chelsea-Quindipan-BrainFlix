9014d06e0b6425a3878ae1aa754eeeafb3cfc5a6
Chelsea Quindipan <quindipc@gmail.com>
2023-06-19 21:38:29 -0400

    add event handling in Header - no state & index link on logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,33 +1,53 @@
-import './Header.scss';
+import "./Header.scss";
 import brainflixLogo from "../../assets/images/logo/BrainFlix-logo.svg";
 import brainflixAvatar from "../../assets/images/avatar/Mohan-muruge.jpg";
 
 export default function Header() {
-    return (
-     <>
-        <header className="header">
-                <img
-                    className="header__logo"
-                    src={brainflixLogo} 
-                    alt="BrainFlix Logo"
-                />
-                <form className="header__form">
-                    <div className="header__search-container">
-                        <input className="header__form--search" type="text" placeholder="Search"></input>
-                    <img
-                        className="header__form--avatar"
-                        src={brainflixAvatar} 
-                        alt="BrainFlix Avatar"
-                    />
-                    </div>
-                    <button className="header__form--upload">Upload</button>
-                    <img
-                        className="header__form--avatar-tablet"
-                        src={brainflixAvatar} 
-                        alt="BrainFlix Avatar"
-                    />
-                </form> 
+  const uploadHandler = (event) => {
+    console.log(event);
+    event.preventDefault();
+  };
+
+  const searchHandler = (event) => {
+    const searchTerm = event.target.value;
+    console.log("Search term:", searchTerm);
+  };
+
+  return (
+    <>
+      <header className="header">
+        <a className="header__logo-index" href="../../index.js">
+          <img
+            className="header__logo"
+            src={brainflixLogo}
+            alt="BrainFlix Logo"
+          />
+        </a>
+        <form
+          onClick={(event) => uploadHandler(event)}
+          className="header__form"
+        >
+          <div className="header__search-container">
+            <input
+              className="header__form--search"
+              type="text"
+              placeholder="Search"
+              onChange={(event) => searchHandler(event)}
+            ></input>
+            <img
+              className="header__form--avatar"
+              src={brainflixAvatar}
+              alt="BrainFlix Avatar"
+            />
+          </div>
+          <button className="header__form--upload">Upload</button>
+          <img
+            className="header__form--avatar-tablet"
+            src={brainflixAvatar}
+            alt="BrainFlix Avatar"
+          />
+        </form>
       </header>
-     </>
-    );
-  }
+    </>
+  );
+}
