62202bac09dddcba457d3553a81c478dd4cb9ba9
Chelsea Quindipan <quindipc@gmail.com>
2023-06-21 16:12:40 -0400

    Add dateFormat npm and remove unnecessary Timestamp scss

--- src/components/video/timestamp/Timestamp.jsx
+++ src/components/video/timestamp/Timestamp.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
-import "./Timestamp.scss";
 import details from "../../../data/video-details.json";
+import dateFormat, { masks } from "dateformat";
+
 
 export default function Timestamp() {
   const [detailsData, setDetailsData] = useState({});
@@ -9,36 +10,14 @@ export default function Timestamp() {
     setDetailsData(details[0]);
   }, []);
 
-  const date = {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-    timeZone: "America/Toronto",
-    };
-      
-    
-    return (
+  const formatTimestamp = Date();
+
+
+  console.log(dateFormat(formatTimestamp, "paddedShortDate"));
+  
+return (
         <>
-      <p className="videoinfo__info">{detailsData.timestamp}</p>
+      <p className="videoinfo__info">{detailsData.timestamp}</p> 
     </>
   );
 }
-    // Get the current date in the format of MM/DD/YYYY
-// function getCurrentDate() {
-//     const currentDate = new Date();
-//     const month = currentDate.getMonth() + 1;
-//     const day = currentDate.getDate();
-//     const year = currentDate.getFullYear();
-    // const hours = currentDate.getHours();
-    // const minutes = currentDate.getMinutes();
-    // const seconds = currentDate.getSeconds();
-
-    // Timestamps ago
-    // if (hours < 60) {
-    //     return `${day}/${month}/${year} - Posted ${seconds} seconds ago`;
-    // } else if (minutes < 60) {
-    //     return `${day}/${month}/${year} - Posted ${minutes} minutes ago`;
-    // } else {
-    //     return `${day}/${month}/${year} - Posted ${hours} hours ago`;
-    // }
-// }
