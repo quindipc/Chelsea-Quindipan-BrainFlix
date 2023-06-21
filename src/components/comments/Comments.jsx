5a7f9775d53f19f233dd4e6a335c7d744f008796
Chelsea Quindipan <quindipc@gmail.com>
2023-06-21 13:47:49 -0400

    start comments component

---
+++ src/components/comments/Comments.jsx
@@ -0,0 +1,16 @@
+import "./Comments.scss";
+
+
+export default function Comments() {
+    // const [videoData, setVideoData] = useState({});
+  
+    // useEffect(() => {
+    //   setVideoData(videos[0]);
+    // }, []);
+  
+    return (
+        <>
+         
+        </>
+      );
+  }
