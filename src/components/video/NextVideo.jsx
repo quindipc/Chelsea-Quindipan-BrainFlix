b571b1d8c7da412d9e73c42b2084e62994dc6d78
Chelsea Quindipan <quindipc@gmail.com>
2023-06-22 21:07:04 -0400

    Rearrange video file folder structure

--- src/components/video/NextVideo.jsx
+++ src/components/video/NextVideo.jsx
@@ -1,18 +1,18 @@
 import "./NextVideo.scss";
-import NextVideoItem from "./NextVideoItem";
-import videos from "../../../data/videos.json";
+// import NextVideoItem from "./NextVideoItem";
+// import videos from "../../data/videos.json";
 
 export default function NextVideo() {
   return (
     <>
-      <section className="nextvideo__section">
+      {/* <section className="nextvideo__section">
         <h3 className="nextvideo__title">Next videos</h3>
 
         {videos.map((video) => (
           <NextVideoItem key={video.id} videoData={video} />
         ))}
 
-      </section>
+      </section> */}
     </>
   );
 }
---
+++ src/components/video/NextVideoItem.jsx
@@ -0,0 +1,11 @@
+import "./NextVideoItem.scss";
+
+export default function NextVideoItem() {
+  return (
+    <div className="nextvideoitem">
+      {/* <video poster={videoData.image} className="nextvideoitem__video"></video>
+      <h3 className="nextvideoitem__title">{videoData.title}</h3>
+      <p className="nextvideoitem__channel">{videoData.channel}</p> */}
+    </div>
+  );
+}
