09f1e95b6f240279907122d678bd5be0c17442a0
Chelsea Quindipan <quindipc@gmail.com>
2023-07-01 20:26:51 -0400

    Add upload page CSS styles

--- src/pages/UploadPage/UploadPage.jsx
+++ src/pages/UploadPage/UploadPage.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import "./UploadPage.scss";
 
 //Components
 import Header from "../../components/Header/Header";
@@ -26,11 +27,14 @@ export default function UploadPage() {
 
     setError(false);
 
-    //Return to homepage
-    setTimeout(() => {
-      setSuccess(true);
-      navigate("/");
-    }, 3000);
+
+    // //Return to homepage
+    // setTimeout(() => {
+    //   setSuccess(true);
+    //   navigate("/");
+    // }, 3000);
+
+    setSuccess(true);
   };
 
   const cancelHandler = (event) => {
@@ -48,24 +52,65 @@ export default function UploadPage() {
   return (
     <>
       <Header />
-      <h1 className="upload__title">Upload Video</h1>
-      <h2 className="upload__subheader">Video Thumbnail</h2>
-      <img className="upload__thumbnail" src={thumbnail} alt="bike thumbnail" />
-      <form onSubmit={publishHandler}>
-        <label className="upload__subheader" htmlFor="">
-          Title Your Video
-        </label>
-        <input className="upload__input" type="text" name="title" />
-        <label className="upload__subheader">Add a video description</label>
-        <input className="upload__input" type="text" name="description" />
-        {showError && <div>Please fill out all the fields</div>}
-        <button type="submit">Publish</button>
-        <button onClick={cancelHandler}>Cancel</button>
-      </form>
-
-      {showSuccess && (
-        <div className="success-notification">Upload successful!</div>
-      )}
+      <div className="upload">
+        <h1 className="upload__title">Upload Video</h1>
+        <hr className="upload__divider--desktop" />
+        <div className="upload__container--desktop">
+          <div className="upload__container">
+            <h2 className="upload__label">Video Thumbnail</h2>
+            <img
+              className="upload__thumbnail"
+              src={thumbnail}
+              alt="bike thumbnail"
+            />
+          </div>
+          <form className="upload__form" onSubmit={publishHandler}>
+            <label className="upload__label" htmlFor="">
+              Title Your Video
+            </label>
+            <input
+              className="upload__input upload__input--title"
+              type="text"
+              name="title"
+              placeholder="Add a title to your video"
+            />
+            <label className="upload__label">Add a video description</label>
+            <input
+              className="upload__input upload__input--description"
+              type="text"
+              name="description"
+              placeholder="Add a description to your video"
+            />
+            {showError && (
+              <div className="upload__error">
+                Please fill out all the fields.
+              </div>
+            )}
+            <hr className="upload__divider--tablet" />
+            <div className="upload__container--form">
+              <div className="upload__container--actions">
+                <button className="upload__publish" type="submit">
+                  Publish
+                </button>
+                <p className="upload__cancel" onClick={cancelHandler}>
+                  Cancel
+                </p>
+              </div>
+            </div>
+          </form>
+          {showSuccess && (
+                <div className="upload__success">
+                  Upload successful!
+                </div>
+          )}
+        </div>
+        {showSuccess && (
+                <div className="upload__success--desktop">
+                  Upload successful!
+                </div>
+          )}
+        <hr className="upload__divider--form" />
+      </div>
     </>
   );
 }
